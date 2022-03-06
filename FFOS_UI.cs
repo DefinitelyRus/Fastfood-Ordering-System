435968c879ab0c4113c33cf3b44df495bd24095f
DefinitelyRus <definitelyrus@gmail.com>
2022-03-06 18:35:14 +0800

    Experimentation with DataGrids

--- FFOS_UI.cs
+++ FFOS_UI.cs
@@ -61,7 +61,34 @@ namespace FastfoodOrderingSystem
 
         private void formUI_Shown(object sender, EventArgs e)
         {
+            menuDataGrid.RowCount = 100;
+            //Make this scalable with the number of actual menu items.
 
+            Console.WriteLine(menuDataGrid.RowCount);
+            //menuDataGrid[0, 0] = null;
+            //menuDataGrid[1, 0] = null;
+            //menuDataGrid[2, 0] = null;
+            for (int rowIndex = 0; rowIndex < menuDataGrid.Rows.Count; rowIndex++)
+            {
+                /* 
+                 * Column Index guide:
+                 * 0: Image
+                 * 1: Name
+                 * 2: Price
+                 */
+                string imgPath = Environment.ProcessPath;
+                Console.WriteLine(imgPath);
+                //Image img = Image.FromFile(imgPath);
+                DataGridViewCell imgCell = menuDataGrid[0, rowIndex];
+                DataGridViewCell nameCell = menuDataGrid[1, rowIndex];
+                DataGridViewCell priceCell = menuDataGrid[2, rowIndex];
+
+                Console.WriteLine("");
+                Console.WriteLine(imgCell.ValueType);
+                Console.WriteLine(nameCell.ValueType);
+                Console.WriteLine(priceCell.ValueType);
+            }
+            //menuDataGrid[1,4] = null;
         }
     }
 }
